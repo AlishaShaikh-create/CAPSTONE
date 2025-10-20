3ca10e932b0c68519545c53f59a9bc170d0523f3
AlishaShaikh-create <alishashaikh186555@gmail.com>
2025-10-20 13:07:22 +0530

    jwt token

--- server/workers/sendSMS.js
+++ server/workers/sendSMS.js
@@ -1,6 +1,6 @@
 import twilio from 'twilio';
-const accountSid = "AC44a7d4469142fd1ee303d724f2d041a2"
-const authToken = "f9e84058ec849f568a1e79087a70760f"
+const accountSid = SID
+const authToken = TOKEN 
 const client = new twilio (accountSid , authToken);
 
 async function sendSMS(smsData){
